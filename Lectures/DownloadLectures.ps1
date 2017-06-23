0cf0d4261ebbbf3482dbe36f08decc696d6e7e84
Jye <jyesawtellrickson@gmail.com>
2017-06-23 15:15:24 +0800

    Update DownloadLectures.ps1

--- Lectures/DownloadLectures.ps1
+++ Lectures/DownloadLectures.ps1
@@ -1,1 +1,1 @@
-﻿for ($i=1;$i -le 18;$i++){ (New-Object Net.WebClient).DownloadFile('https://d396qusza40orc.cloudfront.net/ml/docs/slides/Lecture'+$i+'.pdf', 'C:\home\jye\docs\machine_learning_stanford\Lectures\Lecture_'+$i+'.pdf')}
+﻿for ($i=1;$i -le 18;$i++){ (New-Object Net.WebClient).DownloadFile('https://d396qusza40orc.cloudfront.net/ml/docs/slides/Lecture'+$i+'.pdf', 'C:\Lectures\Lecture_'+$i+'.pdf')}
